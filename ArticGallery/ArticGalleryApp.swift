389a17348aefdbd9577feb2fda3664e0b34e3380
Ariel Goldfryd <agoldfryd@proton.me>
2023-12-19 10:28:00 -0300

    Initial Commit

---
+++ ArticGallery/ArticGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticGalleryApp.swift
+//  ArticGallery
+//
+//  Created by Ariel on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArticGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
